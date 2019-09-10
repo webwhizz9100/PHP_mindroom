5c7ed6e6a5075c649ee45f3eec05f4eae541807e
Nat <x5_452_dark@hotmail.com>
2019-09-10 18:20:04 +1000

    dynamic app

---
+++ app/public/db.info.php
@@ -0,0 +1,65 @@
+<?php 
+
+$post=[
+    
+        [
+            "id"=> 3363,
+            "title"=> "The 3-minute guide to boosting posts on Facebook.",
+            "excerpt"=> "Learn everything important about boosting posts on Facebook in just 3 minutes or less! We cover what they are, how to do it, when you should be boosting and when you shouldn't. ",
+            "date"=> "Fri Aug 16 2019 15:31:40 +1000",
+            "category"=> "Digital Marketing",
+            "url"=> "https://lsb2bdev.wpengine.com/blog/the-3-minute-guide-to-boosting-posts-on-facebook/",
+            "image"=> "https://lsb2bdev.wpengine.com/wp-content/uploads/2019/08/Boosting-posts-on-Facebook-to-earn-money-400x300.png",
+            "author_name"=> "Myles Hart",
+            "author_avatar"=> "https://secure.gravatar.com/avatar/b16382efc86514283df213573b8544d5?s=96&d=mm&r=g"
+
+        ],
+        [
+            "id"=> 3171,
+            "title"=> "Localsearch Presents the Gold Coast Titans Derby Luncheon (July 2019)",
+            "excerpt"=> "Localsearch was proud to partner with the Gold Coast Titans in the 2019 Derby Luncheon. Held on the 26 July 2019, the event saw digital marketing experts, business owners and NRL greats unite under one roof.",
+            "date"=> "Thu Aug 01 2019 13:32:28 +1000",
+            "category"=> "Media",
+            "url"=> "https://lsb2bdev.wpengine.com/blog/localsearch-presents-the-gold-coast-titans-derby-luncheon-july-2019/",
+            "image"=> "https://lsb2bdev.wpengine.com/wp-content/uploads/2019/08/Localsearch-presents-the-2019-Gold-Coast-Titans-Derby-Luncheon-400x300.jpg",
+            "author_name"=> "Sarah Russo",
+            "author_avatar"=> "https://secure.gravatar.com/avatar/c80ac45ebda2014a8a5196ea0ff29a0b?s=96&d=mm&r=g"
+        ],
+        [
+            "id"=> 3149,
+            "title"=> "How much does a website cost in Australia? The truth.",
+            "excerpt"=> "With the price of websites being so varied, it's easy for website design companies to take advantage of unknowing businesses. Find out what you need to know about building a professional website in 'How much does a website cost in Australia? The truth'. ",
+            "date"=> "Fri Jul 26 2019 15:49:07 +1000",
+            "category"=> "Digital Marketing",
+            "url"=> "https://lsb2bdev.wpengine.com/blog/how-much-does-a-website-cost-in-australia-the-truth/",
+            "image"=> "https://lsb2bdev.wpengine.com/wp-content/uploads/2019/07/How-much-does-a-website-cost-in-Australia-400x300.jpg",
+            "author_name"=> "Sarah Russo",
+            "author_avatar"=> "https://secure.gravatar.com/avatar/c80ac45ebda2014a8a5196ea0ff29a0b?s=96&d=mm&r=g"
+        ],
+        [
+            "id"=> 3033,
+            "title"=> "Now Microsoft owns Bing, what does it mean for your online advertising?",
+            "excerpt"=> "What does Microsoft owning Bing have to do with you and your business's digital marketing? Well, for one, it means you could be saving a bucket load on your search engine marketing. How? Give this article a read and find out!",
+            "date"=> "Thu Jul 11 2019 15:12:19 +1000",
+            "category"=> "Digital Marketing",
+            "url"=> "https://lsb2bdev.wpengine.com/blog/now-microsoft-owns-bing-what-does-it-mean-for-your-online-advertising/",
+            "image"=> "https://lsb2bdev.wpengine.com/wp-content/uploads/2019/07/Microsoft-owns-Bing-in-new-merger-Localsearch-400x300.jpg",
+            "author_name"=> "Sarah Russo",
+            "author_avatar"=> "https://secure.gravatar.com/avatar/c80ac45ebda2014a8a5196ea0ff29a0b?s=96&d=mm&r=g"
+        ],[
+            "id"=>2994,
+        "title"=>"It's a date! The Mobile-First Google Algorithm is coming 1 July 2019.",
+        "excerpt"=>"Is your website running mobile and tablet viewing best practices? If not, or you're not sure, you could fall victim to the Mobile-First Google Algorithm rolling out in July. We tell you everything you need to know.",
+        "date"=>"Fri Jun 21 2019 09:51:46 +1000",
+        "category"=>"Digital Marketing",
+        "url"=>"https://lsb2bdev.wpengine.com/blog/its-a-date-the-mobile-first-google-algorithm-is-coming-1-july-2019/",
+        "image"=>"https://lsb2bdev.wpengine.com/wp-content/uploads/2019/06/Warning-mobile-first-Google-algorithm-coming-1-July-2019-400x300.png",
+        "author_name"=>"Sarah Russo",
+        "author_avatar"=>"https://secure.gravatar.com/avatar/c80ac45ebda2014a8a5196ea0ff29a0b?s=96&d=mm&r=g"
+        ]
+    
+];
+
+// $title = $post[0]['title'];
+
+// echo "<h1>($title)</h1>";
--- app/public/index.php
+++ app/public/index.php
@@ -1,30 +1,40 @@
-<?php
+<?php 
+include 'db.info.php'?>
 
-$posts=[
-    [
-        "Author" => "Charls Dickens",
-        "Content" => "Christmas Carrols"
-],
-    [
-        "Author" => "J.K.Rowling",
-        "Content" =>"Harry Potter"
-    ],
-    [
-        "Author"=> "J.R.R Tolkin",
-        "Content" => "Lord of the Rings"
-    ]
-];
-?>
-<?php foreach($posts as $each) : ?>
-  
-    <div class="card"> 
-        <?php echo $each["Content"];?> was written by
-        <?php echo $each["Author"]; ?>
-    </div>
 
- <?php endforeach ?>
-<style>
-    
-</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+</head>
+<body>
+<ul class="menu">
+    <li><a href="/?author=Myles Hart">Myles</a></li>
+    <li><a href="/?author=Sarah Russo">Sarah</a></li>
+</ul>
+    <?php 
+    $i = 0;
+    foreach($post as $bio):?>
+        <? if(array_key_exists('author',$_GET)){
+            
+            if($bio['author_name'] !=$_GET['author'])continue;
+        }
+            ?>
 
+        <li>
+            <h1><?= $bio['title']?></h1>
+            by
+            <em><?= $bio['author_name'] ?></em>
+        </li>
+       
+       
+
+   
+
+
+<?php endforeach?>
+</body>
+</html>
  
